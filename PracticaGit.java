412a2ac537b26fffe869d867a639f5196769bf0e
juanmaca119-ai <juanmaca119@gmail.com>
2026-02-03 09:15:38 +0100

    Update PracticaGit.java

--- PracticaGit.java
+++ PracticaGit.java
@@ -9,5 +9,18 @@ public class PracticaGit {
         String opcion = sc.nextLine();
 
         System.out.println("Usted está: "+opcion);
+
+        //Modificado en GitHub
+        System.out.printl("¿Está seguro que se siente bien?");
+        String opcion2 = sc.nextLine().toLowerCase;
+
+        if (opcion2.equals("si")) {
+            System.out.println("Usted no está bien");
+        } else {
+            System.out.println("Gracias por no mentir");
+        }
+
+        
+        
     }
 }
