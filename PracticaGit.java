711de436a52eda953ba80973261557618ef8379c
JuanManuelCabreraL <juanmaca119@gmail.com>
2026-02-03 08:52:28 +0100

    Commit2 Iniciando

---
+++ PracticaGit.java
@@ -0,0 +1,13 @@
+package PracticaGitHub;
+
+import java.util.Scanner;
+
+public class PracticaGit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cómo estás? ");
+        String opcion = sc.nextLine();
+
+        System.out.println("Usted está: "+opcion);
+    }
+}
